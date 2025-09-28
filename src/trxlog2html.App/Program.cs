3c39ba08af4a1c9e18525af2b50c179f8dabd880
Kostiantyn <usenkoko@ukr.net>
2025-09-28 14:08:48 +0200

    Refactor (#1)
    * Add detailed test result summary and error handling in report
    * Update package output path and adjust .gitignore for nupkg
    * create test project.
    refactor ReportModel
    * Add report model and HTML template for test results
    * Add default HTML template for test report generation
    Update HTML template for test report with language attribute and style adjustments
    Enhance TRX report generation with improved XML parsing and HTML template updates

---
+++ src/trxlog2html.App/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using CommandLine;
+using RazorEngine;
+using RazorEngine.Templating;
+using trxlog2html;
+using Encoding = System.Text.Encoding;
+
+namespace Trxlog2Html;
+
+public class Program {
+
+    private static async Task Main(string[] args) {
+        ParserResult<Options> arguments = Parser.Default.ParseArguments<Options>(args);
+
+        if (arguments.Errors.Any()) {
+            Console.WriteLine("Invalid arguments.");
+
+            foreach (Error error in arguments.Errors) {
+                Console.WriteLine(error);
+            }
+
+            return;
+        }
+
+        string templateFilePath = string.IsNullOrEmpty(arguments.Value.TemplateFilePath)
+            ? Path.Combine(GetBuiltInTemplatesDir(), "default-template.cshtml")
+            : arguments.Value.TemplateFilePath;
+        string template = ReadTemplate(templateFilePath);
+        string outputFilePath = string.IsNullOrEmpty(arguments.Value.OutputFilePath)
+            ? templateFilePath + ".html"
+            : arguments.Value.OutputFilePath;
+
+        Console.WriteLine("Reading the file...");
+        await using FileStream stream = new (arguments.Value.InputFilePath, FileMode.Open, FileAccess.Read);
+        XmlSerializer serializer = new (typeof(TestRun));
+        Console.WriteLine("Parsing the file...");
+        TestRun result = (TestRun)serializer.Deserialize(stream);
+
+        Console.WriteLine("Generating the report...");
+        string html = Engine.Razor.RunCompile(template, "templateKey", typeof(TestRun), result);
+        Console.WriteLine("Writing the result...");
+        WriteResult(outputFilePath, html);
+        Console.WriteLine("Done.");
+    }
+
+    private static string GetBuiltInTemplatesDir() {
+        return Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Templates");
+    }
+
+
+    private static string ReadTemplate(string path) {
+        using StreamReader reader = new (path, Encoding.UTF8);
+        return reader.ReadToEnd();
+
+    }
+
+    private static void WriteResult(string path, string result) {
+        using StreamWriter writer = new (path, false, Encoding.UTF8);
+        writer.Write(result);
+    }
+
+    public class Options {
+        [Option('i', "input", Required = true, HelpText = "Input file path.")]
+        public string InputFilePath { get; set; }
+
+        [Option('o', "output", Required = false, HelpText = "Output file path.")]
+        public string OutputFilePath { get; set; }
+
+        [Option('t', "template", Required = false, HelpText = "Template file path.")]
+        public string TemplateFilePath { get; set; }
+    }
+}
